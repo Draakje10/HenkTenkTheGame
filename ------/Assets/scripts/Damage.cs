5ef82ef379c27275328f19c18e39adbcdd87778c
Draakje10 <58084526+Draakje10@users.noreply.github.com>
2020-04-30 16:06:14 +0200

    Alpha 1
    features main menu and working ai

---
+++ ------/Assets/scripts/Damage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour
+{
+    public GameObject Enemy;
+    // Use this for initialization
+    void OntriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "theBullet")
+        {
+            Destroy(Enemy);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
