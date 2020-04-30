9942f8d766ae3ed733858dc41ba474014b1f658d
Draakje10 <58084526+Draakje10@users.noreply.github.com>
2020-04-30 11:41:02 +0200

    AI added

---
+++ ------/Assets/new ai/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* Controls the Enemy AI */
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;  // Detection range for player
+
+    Transform target;   // Reference to the player
+    NavMeshAgent agent; // Reference to the NavMeshAgent
+
+    // Use this for initialization
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance to the target
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        // If inside the lookRadius
+        if (distance <= lookRadius)
+        {
+            // Move towards the target
+            agent.SetDestination(target.position);
+        }
+    }
+
+
+
+    // Show the lookRadius in editor
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
