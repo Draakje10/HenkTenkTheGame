2c8fdf02bb1ac172960f554eca80ce8fc0c65ea6
Draakje10 <58084526+Draakje10@users.noreply.github.com>
2020-04-30 17:54:14 +0200

    nietwerkende ui

---
+++ ------/Assets/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject IngameHud;
+    public GameObject Player;
+    public GameObject ak;
+    public GameObject grappler;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+            Application.Quit;
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        IngameHud.SetActive(true);
+        Player.SetActive(true);
+        ak.SetActive(true);
+        grappler.SetActive(true);
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        IngameHud.SetActive(false);
+        Player.SetActive(false);
+        ak.SetActive(false);
+        grappler.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }                          
+}
