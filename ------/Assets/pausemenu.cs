385105ce0b4ed3480a50ecd3e4388eda70a1f6e9
Draakje10 <58084526+Draakje10@users.noreply.github.com>
2020-05-01 16:12:51 +0200

    alpha 2 working

---
+++ ------/Assets/pausemenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pausemenu : MonoBehaviour
+{
+   
+    public static bool PausedGame = false;
+
+    public GameObject PauseMenuUI;
+    public GameObject PlayerUI;
+    public GameObject Player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedGame = false;
+        Player.SetActive(true);
+        PlayerUI.SetActive(true);
+    }
+
+    void Pause ()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PausedGame = true;
+        Player.SetActive(false);
+        PlayerUI.SetActive(false);
+       
+    }
+
+
+
+
+
+}
---
+++ ------/Assets/scripts/Anima.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anima : MonoBehaviour
+{
+        // Start is called before the first frame update
+        protected Animator Animation;
+
+        protected void Awake()
+        {
+            Animation = GetComponent<Animator>();
+        }
+
+        protected void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+            Animation.SetBool("Shooting", true);
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+            Animation.SetBool("Shooting", false);
+            }
+        }
+}
