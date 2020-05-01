bb9a8b32e361a2da80943755bc2ccd50f93ac767
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2020-05-01 17:49:37 +0200

    dumbv ai

---
+++ ------/Assets/scripts/SpiderAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* Controls the Enemy AI */
+
+public class SpiderAI : MonoBehaviour
+{
+    [Header("Parameters")]
+    public float lookRadius = 20f;  // Detection range for player
+
+    [Header("References")]
+    
+    public Transform target;   // Reference to the player
+    
+    NavMeshAgent agent; // Reference to the NavMeshAgent
+
+    // Use this for initialization
+    void Start()
+    {
+        //target = PlayerManager.instance.player.transform; //doesnt work right now
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance to the target
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        // If inside the lookRadius
+        if (distance <= lookRadius)
+        {
+            // Move towards the target
+            agent.SetDestination(target.position);
+            Debug.Log("Spider spotted player");
+        }
+    }
+
+
+
+    // Show the lookRadius in editor
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
